b9f200dab78705a3baba282f23dfee0b96e1d099
J.D. Flynn <jflynn8@gmail.com>
2017-06-13 14:16:06 -0500

    fixing the base page

--- slides/index.php
+++ slides/index.php
@@ -7,8 +7,9 @@ $dh = opendir($dir);
 <?php
 while ($f = readdir($dh)) {
   $fullpath = $dir ."/". $f;
+  $name = str_replace('-', ' ', $f);
   if ($f{0} == "." || !is_dir($fullpath)) continue;
-  echo "<li><a href=\"$fullpath\">" . ucwords($f) . "</a></li>\n";
+  echo "<li><a href=\"$fullpath\">" . ucwords($name) . "</a></li>\n";
 }
 closedir($dh);
 ?>
